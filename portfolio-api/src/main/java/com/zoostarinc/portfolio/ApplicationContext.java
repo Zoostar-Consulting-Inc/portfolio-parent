2492a7995ebde278bf4f40017f61ad3ca49c5052
zoostar <devops@zoostar.net>
2026-07-20 16:35:23 -0400

    POR-1: Develop feature 1.0.0

--- portfolio-api/src/main/java/com/zoostarinc/portfolio/ApplicationContext.java
+++ portfolio-api/src/main/java/com/zoostarinc/portfolio/ApplicationContext.java
@@ -2,6 +2,7 @@ package com.zoostarinc.portfolio;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.config.Customizer;
@@ -22,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 @Configuration
 @EnableAspectJAutoProxy
 @RequiredArgsConstructor
+@ComponentScan(basePackages = { "net.zoostar", "com.zoostarinc" })
 //@EnableTransactionManagement
 public class ApplicationContext {
 
