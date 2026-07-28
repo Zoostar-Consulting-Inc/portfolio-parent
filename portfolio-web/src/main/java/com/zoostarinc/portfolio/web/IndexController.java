f1a6ca6e8f992c5c2a5218dd54cb976559ce5cc7
zoostar <devops@zoostar.net>
2026-07-28 16:43:45 -0400

    POR-6: Develop feature 1.4.0

--- portfolio-web/src/main/java/com/zoostarinc/portfolio/web/IndexController.java
+++ portfolio-web/src/main/java/com/zoostarinc/portfolio/web/IndexController.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,10 +19,10 @@ public class IndexController {
 	private String apiBaseUrl;
 
 	@GetMapping(path = "/")
-	public String index(@AuthenticationPrincipal OidcUser user, HttpSession session) {
+	public String index(@AuthenticationPrincipal OidcUser user, Model model) {
 		log.info("Hello User: {}", user);
-		session.setAttribute("name", user.getFullName());
-		session.setAttribute("apiBaseUrl", apiBaseUrl);
+		model.addAttribute("name", user.getFullName());
+		model.addAttribute("apiBaseUrl", apiBaseUrl);
 		return "index";
 	}
 }
